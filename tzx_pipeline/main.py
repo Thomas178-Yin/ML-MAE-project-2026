090b466a59ec209924890a35f4bcf93a97d4d4bf
tzx242 <12413304@mail.sustech.edu.cn>
2026-05-11 21:12:11 +0800

    A copy for moderation
    这是一个用于优化性能的pipeline的副本。

---
+++ tzx_pipeline/main.py
@@ -0,0 +1,129 @@
+import os
+import sys
+import torch
+import yaml
+import argparse
+import pandas as pd
+
+from util import set_seed, path_solution, config_fix, val_matrix
+#——————————————————
+# 解决导入路径
+#——————————————————
+path_solution(__file__)
+
+from dataset import get_dataloader
+from models.model_EEGNet import EEGNet
+from models.model_iTransformer import iTransformer
+from models.model_PatchTST import PatchTST
+from models.model_TimesNet import TimesNet
+from models.model_EEGGRU import EEGGRU
+from models.model_CBraMod import CBraMod
+from train import train, test
+
+#——————————————————
+# 选择数据集
+#——————————————————
+DATASET_LIST = ['MDD', 'BCIC2A', 'CHINESE', 'SEED', 'SLEEP']
+dataset_id = 0
+dataset_name = DATASET_LIST[dataset_id]
+
+MODEL_LIST = [EEGNet, EEGGRU, iTransformer, PatchTST, TimesNet, CBraMod]
+#——————————————————
+# 选择模型
+#——————————————————
+model_id = 3
+model = MODEL_LIST[model_id]
+model_name = MODEL_LIST[model_id].__name__
+print(model_name)
+#——————————————————
+# 设置随机参数
+#——————————————————
+seed = 42
+
+DATASET_PATH = f"C:/Users/ASUS/Desktop/machine_learning/course_project/{dataset_name}"  
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--data_dir', type=str, default=DATASET_PATH , help='data_dir')
+
+parser.add_argument('--model_save_dir', type=str, default=f"C:/Users/ASUS/Desktop/machine_learning/course_project/results/{dataset_name}/{model_name}", help='model save dir')
+args = parser.parse_args()
+print(args)
+
+# 检查并创建保存目录
+os.makedirs(args.model_save_dir, exist_ok=True)
+
+project_root = os.path.dirname(os.path.dirname(__file__))  # 适用于模块化项目
+config_path = os.path.join(project_root, "tzx_pipeline", "config", f"base_{model_name}.yaml")
+
+with open(config_path, "r", encoding='utf-8') as f:
+    config = yaml.safe_load(f)
+    # 提取需要的键
+    keys = ["train", "model"]
+    # 使用列表推导式 + join() 安全拼接, 
+    output = "\n".join(f"{key}: {config.get(key, '')}" for key in keys)
+    print(output)
+
+# 针对数据集修复config参数
+config = config_fix(config, dataset_name, model_name)
+
+# 保存yaml文件到模型保存路径 方便查看
+save_config_path = os.path.join(args.model_save_dir, f"config_used_{dataset_name}_{model_name}.yaml")
+with open(save_config_path, "w") as f:
+    yaml.dump(config, f)
+print(f"config save to: {save_config_path}")
+
+
+#————————————————————
+#训练过程
+#————————————————————
+
+# 固定种子
+set_seed(seed = seed)
+
+dataloader_train, dataloader_val, dataloader_test = get_dataloader(args = args, batch_size = config['dataset']['batch_size'], seed = seed)
+
+print("Num batches:")
+print(f"  Train: {len(dataloader_train)}")
+print(f"  Val:   {len(dataloader_val)}")
+print(f"  Test:  {len(dataloader_test)}")
+
+best_model, best_val_acc = train(
+                            model            = model,
+                            train_loader     = dataloader_train,
+                            val_loader       = dataloader_val, 
+                            dataset          = dataset_name,    
+                            config           = config,
+                            seed             = seed,
+                                )
+
+predictions = test(
+                    best_model_state= best_model,
+                    model           = model,    
+                    config          = config, 
+                    test_loader     = dataloader_test,
+                    # seed            = seed,
+                    )
+
+# 保存模型
+save_path = os.path.join(args.model_save_dir, f"best_model.pt")
+torch.save(best_model, save_path)
+print(f"[✔] Model saved to {save_path}")
+
+# ————————————————————
+# 保存预测结果为 CSV
+# ————————————————————
+csv_save_path = os.path.join(args.model_save_dir, f"predictions_{dataset_name}_{model_name}.csv")
+df_preds = pd.DataFrame({"Prediction": predictions})
+df_preds.to_csv(csv_save_path, index=False)
+print(f"[✔] Predictions saved to {csv_save_path}")
+
+# ————————————————————
+# 汇总val结果
+# ————————————————————
+val_matrix(
+    val_acc = best_val_acc,
+    model_name = model_name,
+    dataset_name = dataset_name,
+    model_save_dir = args.model_save_dir
+)
---
+++ tzx_pipeline/train.py
@@ -0,0 +1,170 @@
+import os
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import torch.optim as optim
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+from util import compute_metrics, init_model
+
+
+def train(  model,
+            train_loader,
+            val_loader,
+            dataset,
+            config,
+            seed = 42):
+    
+    run_dir = f"origin_pipeline/log/{dataset}/{model.__name__}/run_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+
+    writer = SummaryWriter(log_dir=run_dir)
+
+    device = config['train']['device']
+
+    model = init_model(model, config)
+
+    # for p in model.parameters():
+    #         p.requires_grad = True
+
+    optimizer = optim.Adam(
+                        list(model.parameters()),
+                        lr=config['train']['lr'],
+                        weight_decay=config['train'].get('weight_decay', 1.0e-4)
+                        )
+
+    criterion = nn.CrossEntropyLoss()
+
+    # -------------------------
+    # 初始化
+    # -------------------------
+    train_losses = []
+    val_losses = []
+    val_accuracies = []
+
+    patience = config['train']["early_stop_patience"]
+    min_delta = config['train']["early_stop_min_delta"]
+
+    best_val_acc = None
+    best_model = None
+    bad_epochs = 0
+    
+    train_loss_sum = 0.0
+    train_num = 0
+
+    for epoch in tqdm(range(config['train'].get("epochs", 100)), desc="Epochs", position=0, leave=False):
+        model.train()
+
+        epoch_loss = 0.0
+
+        for data, label in tqdm(train_loader, desc="Batches", position=1, leave=False):
+            data    = data.to(device)
+            label   = label.to(device)
+
+            optimizer.zero_grad()
+
+            output = model(data)
+
+            loss = criterion(output, label)
+
+            loss.backward()
+            optimizer.step()
+
+            batch_size = label.size(0)
+            train_loss_sum += loss.item() * batch_size
+            train_num += batch_size
+            epoch_loss += loss.item()
+
+
+        epoch_train_loss = train_loss_sum / train_num
+        train_losses.append(epoch_train_loss)
+
+        num_batches = len(train_loader)
+        avg_epoch_loss = epoch_loss / num_batches
+
+        writer.add_scalar("Train/loss", avg_epoch_loss, epoch)
+
+        # ================= 验证阶段 =================
+        with torch.no_grad():
+            total_metrics = evaluate(
+                                    model       = model,
+                                    val_loader  = val_loader,
+                                    criterion   = criterion,
+                                    device      = device
+                                 )
+        
+        val_loss    = total_metrics["Loss"]
+        val_acc     = total_metrics["ACC"]
+
+        writer.add_scalar("Val/loss",   val_loss,   epoch)
+        writer.add_scalar("Val/ACC",    val_acc,    epoch)
+
+        if best_val_acc is None or (val_acc - best_val_acc) > min_delta:
+            best_val_acc = val_acc
+            bad_epochs = 0
+
+            best_model = {
+                'epoch': epoch + 1,
+                'model': model.state_dict(),
+            }
+            print(f"[✔] New best model at epoch {epoch+1}, Val_ACC={best_val_acc:.4f}")
+        else:
+            bad_epochs += 1
+
+        if bad_epochs >= patience:
+            print(f"Early stopping triggered at epoch {epoch}")
+            break
+
+    writer.close()
+    return best_model, best_val_acc
+
+def evaluate(model, val_loader, criterion, device):
+    model.eval()
+    total_loss = 0.0
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for data, label in val_loader:
+            data = data.to(device)
+            label = label.to(device)
+
+            output = model(data)
+            loss = criterion(output, label)
+
+            batch_size = label.size(0)
+            total_loss += loss.item() * batch_size
+
+            all_preds.append(output)
+            all_labels.append(label)
+
+    all_preds = torch.cat(all_preds, dim=0)
+    all_labels = torch.cat(all_labels, dim=0)
+
+    # 计算指标
+    metrics = compute_metrics(all_labels, all_preds)
+    # 计算整个 epoch 的平均 loss
+    metrics["Loss"] = total_loss / len(val_loader.dataset)
+
+    return metrics
+
+def test(best_model_state, model, test_loader, config):
+    device = config['train']['device']
+    
+    # 实例化模型
+    model = init_model(model, config)
+    
+    # 挂载最优权重
+    model.load_state_dict(best_model_state['model'])
+    model.to(device)
+    model.eval()
+    
+    predictions = []
+    with torch.no_grad():
+        for data in test_loader:
+            data = data.to(device)
+            
+            output = model(data)
+            pred_class = torch.argmax(output, dim=1)
+            predictions.extend(pred_class.cpu().numpy())
+            
+    return predictions
